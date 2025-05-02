ae55815590727b11f4e6f1c60cbabcd3a24071c9
oussama-ay <ayyouboussama9@gmail.com>
2025-05-02 11:06:10 +0100

    fix ex00

--- ex00/megaphone.cpp
+++ ex00/megaphone.cpp
@@ -13,11 +13,7 @@ int	main(int ac, char **av)
 	if (ac == 1)
 		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *";
 	for (int i = 1; i < ac; i++)
-	{
 		str_toupper(av[i]);
-		if (i != ac - 1)
-			std::cout << " ";
-	}
 	std::cout << std::endl;
 	return (0);
 }
